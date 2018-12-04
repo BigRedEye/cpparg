f3bd0decda352ada6f7b9aeab871c70c9fd5403b
BigRedEye <skvor.sm@gmail.com>
2018-12-04 22:57:46 +0300

    style: clang-format

--- cpparg/cpparg.h
+++ cpparg/cpparg.h
@@ -33,8 +33,8 @@ static constexpr size_t TabWidth = 4;
 class ProcessorError : public std::runtime_error {
 public:
     ProcessorError(const std::string& what_)
-        : std::runtime_error(what_)
-    {}
+        : std::runtime_error(what_) {
+    }
 
     ProcessorError(const std::string& name, const std::string& descr)
         : std::runtime_error("Cannot parse option " + name + ": " + descr) {
@@ -44,19 +44,17 @@ public:
 class Processor {
 public:
     Processor(std::string_view lname)
-        : lname_(lname.begin(), lname.end())
-    {
+        : lname_(lname.begin(), lname.end()) {
     }
 
     Processor(char sname, std::string_view lname)
-        : Processor(lname)
-    {
+        : Processor(lname) {
         sname_ = sname;
     }
 
     template<typename Val>
     Processor& store(Val& val) {
-        handler_ = [this, &val] (std::string_view sv) {
+        handler_ = [this, &val](std::string_view sv) {
             if (flag_) {
                 /* --flag foo */
                 val = true;
@@ -69,16 +67,15 @@ public:
             }
         };
         if (flag_) {
-            disableFlag_ = [&val] {
-                val = false;
-            };
+            disableFlag_ = [&val] { val = false; };
         }
         return *this;
     }
 
     template<typename Handler>
     Processor& handle(Handler&& handler) {
-        static_assert(std::is_invocable_v<Handler, std::string_view>,
+        static_assert(
+            std::is_invocable_v<Handler, std::string_view>,
             "Handler should take std::string_view as the first argument");
         handler_ = std::forward<Handler>(handler);
         return *this;
@@ -121,9 +118,7 @@ private:
     void parse(std::string_view arg = "") {
         if (!handler_) {
             throw std::logic_error(
-                "Cannot parse option " + name() +
-                ": the handler was not set. Use either store() or process()."
-            );
+                "Cannot parse option " + name() + ": the handler was not set. Use either store() or process().");
         }
         if (arg.empty() && !hasDefaultArgument_) {
             throw ProcessorError(name(), "argument required.");
@@ -230,16 +225,16 @@ private:
 private:
     static constexpr char EmptyShortName = '\0';
 
-    bool required_{ false };
-    bool flag_{ false };
+    bool required_{false};
+    bool flag_{false};
 
-    char sname_{ EmptyShortName };
+    char sname_{EmptyShortName};
     std::string lname_;
 
     std::string argType_;
     std::string description_;
     std::string defaultArgument_;
-    bool hasDefaultArgument_{ false };
+    bool hasDefaultArgument_{false};
 
     std::function<void(std::string_view)> handler_;
     std::function<void()> disableFlag_;
@@ -249,8 +244,7 @@ class Parser {
 public:
     Parser(std::string_view program, std::string_view mode = "")
         : program_(program.begin(), program.end())
-        , mode_(mode.begin(), mode.end())
-    {
+        , mode_(mode.begin(), mode.end()) {
     }
 
     Parser& title(std::string_view v) {
@@ -271,14 +265,10 @@ public:
             throw std::logic_error("Cannot add two help options");
         }
         Processor& result = createProcessor(sname, lname);
-        result
-            .description("print this help and exit")
-            .handle(
-                [this] (std::string_view) {
-                    printHelp();
-                    exit(0);
-                }
-            );
+        result.description("print this help and exit").handle([this](std::string_view) {
+            printHelp();
+            exit(0);
+        });
 
         help_ = &result;
 
@@ -308,25 +298,15 @@ public:
 
         /* put required arguments first */
         std::vector<const Processor*> sorted;
-        std::transform(
-            processors_.begin(),
-            processors_.end(),
-            std::back_inserter(sorted),
-            [] (const auto& p) {
-                return std::addressof(p);
-            }
-        );
-        std::ptrdiff_t count = std::count_if(sorted.begin(), sorted.end(), [](const Processor* p) {
-            return !p->isOptional();
+        std::transform(processors_.begin(), processors_.end(), std::back_inserter(sorted), [](const auto& p) {
+            return std::addressof(p);
         });
+        std::ptrdiff_t count =
+            std::count_if(sorted.begin(), sorted.end(), [](const Processor* p) { return !p->isOptional(); });
         std::nth_element(
-            sorted.begin(),
-            sorted.begin() + count,
-            sorted.end(),
-            [] (const Processor* lhs, const Processor*) {
+            sorted.begin(), sorted.begin() + count, sorted.end(), [](const Processor* lhs, const Processor*) {
                 return !lhs->isOptional();
-            }
-        );
+            });
         if (help_) {
             auto it = std::find(sorted.begin(), sorted.end(), help_);
             /* to preserve relative order */
@@ -342,10 +322,8 @@ public:
         std::cerr << "\n\nOptions:\n";
 
         std::vector<std::string> opts;
-        std::transform(sorted.begin(), sorted.end(), std::back_inserter(opts), [] (auto p) {
-            return p->help();
-        });
-        auto normailzeTabs = [&opts] () {
+        std::transform(sorted.begin(), sorted.end(), std::back_inserter(opts), [](auto p) { return p->help(); });
+        auto normailzeTabs = [&opts]() {
             size_t right = 0;
             for (auto& s : opts) {
                 size_t cur = s.find_first_of('\t');
@@ -369,12 +347,12 @@ public:
     }
 
 private:
-    template<typename ...Args>
-    Processor& createProcessor(Args&& ...args) {
+    template<typename... Args>
+    Processor& createProcessor(Args&&... args) {
         processors_.emplace_back(std::forward<Args>(args)...);
         Processor& result = processors_.back();
 
-        auto failIfExists = [] (auto& map, const auto& key, const auto& value) {
+        auto failIfExists = [](auto& map, const auto& key, const auto& value) {
             auto it = map.find(key);
             if (it != map.end()) {
                 std::stringstream err;
@@ -398,7 +376,7 @@ private:
     std::vector<Processor> processors_;
     std::unordered_map<std::string_view, Processor*> long_;
     std::unordered_map<char, Processor*> short_;
-    Processor* help_{ nullptr };
+    Processor* help_{nullptr};
 };
 
 } // namespace cpparg
--- cpparg/main.cpp
+++ cpparg/main.cpp
@@ -6,23 +6,14 @@ int main(int argc, const char** argv) {
     cpparg::Parser parser("test");
     parser.title("cpparg-test -- test for cpparg.");
     std::string s;
-    parser.add('q', "qwe")
-        .store(s)
-        .defaultArgument("str")
-        .argumentType("STRING")
-        .description("some string");
+    parser.add('q', "qwe").store(s).defaultArgument("str").argumentType("STRING").description("some string");
     parser.add('a', "add")
-        .handle([](std::string_view v) {
-            std::cout << v << std::endl;
-        })
+        .handle([](std::string_view v) { std::cout << v << std::endl; })
         .required()
         .argumentType("FILE")
         .description("files to commit");
-    parser.add("delete")
-        .argumentType("DIR")
-        .description("delete directory");
-    parser.add('c')
-        .description("do something");
+    parser.add("delete").argumentType("DIR").description("delete directory");
+    parser.add('c').description("do something");
     parser.add_help('h', "help");
     parser.parse(argc, argv);
     parser.printHelp();
