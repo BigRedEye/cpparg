30143818cd426e4d4a715fe18049e79256d91c07
BigRedEye <skvor.sm@gmail.com>
2018-12-05 01:02:39 +0300

    style: Use snake_case

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,18 +3,22 @@
 #include <string>
 
 int main(int argc, const char** argv) {
-    cpparg::Parser parser("test");
+    cpparg::parser parser("test");
     parser.title("cpparg-test -- test for cpparg.");
     std::string s;
-    parser.add('q', "qwe").store(s).defaultArgument("str").argumentType("STRING").description("some string");
+    parser.add('q', "qwe")
+        .store(s)
+        .default_argument("str")
+        .argument_type("STRING")
+        .description("some string");
     parser.add('a', "add")
         .handle([](std::string_view v) { std::cout << v << std::endl; })
         .required()
-        .argumentType("FILE")
+        .argument_type("FILE")
         .description("files to commit");
-    parser.add("delete").argumentType("DIR").description("delete directory");
+    parser.add("delete").argument_type("DIR").description("delete directory");
     parser.add('c').description("do something");
     parser.add_help('h', "help");
     parser.parse(argc, argv);
-    parser.printHelp();
+    parser.print_help();
 }
